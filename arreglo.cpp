1aaf705e6be20ed9a8dccc29004f5504529eae03
DbaE-Im <danielalcedo55@gmail.com>
2024-12-09 13:44:52 -0500

    first commit

---
+++ arreglo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int arreglo [] = {1,2,3,4,5,6};
+
+
+    for (int i = 0; i < 6; i++)
+    {
+        cout << arreglo [i] << endl;
+    }
+    
+    return 0;
+}
